17236259c433875da13f71d4a8dcdb3399ebec43
Anirudh Gattu <104290279+agattu1@users.noreply.github.com>
2024-09-27 21:53:54 -0400

    Update ani.js

--- ani.js
+++ ani.js
@@ -1,36 +1,36 @@
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 window.onload = function() {
-  const headingElement = document.getElementById('typing-effect');
-  const texts = ['🔺 engineer', '⚽️ athlete', '☁ believer']; // Texts to display
-  let textIndex = 0;
-  let charIndex = 0;
+    const headingElement = document.getElementById('typing-effect');
+    const texts = ['🔺 engineer', '⚽️ athlete', '☁ believer']; // Texts to display
+    let textIndex = 0;
+    let charIndex = 0;
 
-  function typeText() {
-    if (charIndex < texts[textIndex].length) {
-      headingElement.textContent += texts[textIndex].charAt(charIndex);
-      charIndex++;
-      setTimeout(typeText, 100); // Typing speed (adjust as needed)
-    } else {
-      setTimeout(eraseText, 1000); // Delay before erasing the text (adjust as needed)
+    function typeText() {
+        if (charIndex < texts[textIndex].length) {
+            headingElement.textContent += texts[textIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(typeText, 100); // Typing speed (adjust as needed)
+        } else {
+            setTimeout(eraseText, 1000); // Delay before erasing the text (adjust as needed)
+        }
     }
-  }
 
-  function eraseText() {
-    const currentText = headingElement.textContent;
-    if (charIndex > 0) {
-      headingElement.textContent = currentText.slice(0, charIndex - 1);
-      charIndex--;
-      setTimeout(eraseText, 50); // Erasing speed (adjust as needed)
-    } else {
-      textIndex++;
-      if (textIndex >= texts.length) {
-        textIndex = 0; // Restart from the beginning once all texts are typed
-      }
-      setTimeout(typeText, 500); // Delay before typing the next text (adjust as needed)
+    function eraseText() {
+        const currentText = headingElement.textContent;
+        if (charIndex > 0) {
+            headingElement.textContent = currentText.slice(0, charIndex - 1);
+            charIndex--;
+            setTimeout(eraseText, 50); // Erasing speed (adjust as needed)
+        } else {
+            textIndex++;
+            if (textIndex >= texts.length) {
+                textIndex = 0; // Restart from the beginning once all texts are typed
+            }
+            setTimeout(typeText, 500); // Delay before typing the next text (adjust as needed)
+        }
     }
-  }
 
-  // Start the typing effect
-  typeText();
+    // Start the typing effect
+    typeText();
 };
