fe28d2ba3f7139b279999008dddf7f392e72f52b
Anirudh Gattu <104290279+agattu1@users.noreply.github.com>
2024-09-27 21:57:30 -0400

    Update ani.js

--- ani.js
+++ ani.js
@@ -1,36 +1,30 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', function() {
     const headingElement = document.getElementById('typing-effect');
-    const texts = ["🔺 engineer", "⚽️ athlete", "☁ believer"]; // Texts to display
-    let textIndex = 0;
-    let charIndex = 0;
+    const texts = ['🔺 engineer', '⚽️ athlete', '☁ believer']; // Array of texts to display
+    let textIndex = 0;  // Which text is currently being typed
+    let charIndex = 0;  // Which character within that text is currently being typed
 
     function typeText() {
         if (charIndex < texts[textIndex].length) {
             headingElement.textContent += texts[textIndex].charAt(charIndex);
             charIndex++;
-            setTimeout(typeText, 100); // Typing speed (adjust as needed)
+            setTimeout(typeText, 100);  // Typing speed
         } else {
-            setTimeout(eraseText, 1000); // Delay before erasing the text (adjust as needed)
+            setTimeout(eraseText, 1000);  // Delay before erasing
         }
     }
 
     function eraseText() {
-        const currentText = headingElement.textContent;
         if (charIndex > 0) {
-            headingElement.textContent = currentText.slice(0, charIndex - 1);
+            headingElement.textContent = texts[textIndex].slice(0, charIndex - 1);
             charIndex--;
-            setTimeout(eraseText, 50); // Erasing speed (adjust as needed)
+            setTimeout(eraseText, 50);  // Erasing speed
         } else {
-            textIndex++;
-            if (textIndex >= texts.length) {
-                textIndex = 0; // Restart from the beginning once all texts are typed
-            }
-            setTimeout(typeText, 500); // Delay before typing the next text (adjust as needed)
+            textIndex = (textIndex + 1) % texts.length;  // Move to next text, loop back to start
+            setTimeout(typeText, 500);  // Delay before starting to type the next text
         }
     }
 
-    // Start the typing effect
+    // Initialize the typing effect
     typeText();
-};
+});
